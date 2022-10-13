f7bc07bd2b70f3f55e64d3aaec4e56a959f6c7d0
abiolaolaide <ayodeleabiola1994@gmail.com>
2022-10-13 12:14:43 -0700

    C puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
 */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\n"Programming is like building a multilingual puzzle");
 	return (0);
 }
 
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: The program with printf
+ * return: Always 0
+ */
+int main(void)
+i{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
