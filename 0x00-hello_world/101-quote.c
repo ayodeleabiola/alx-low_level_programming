fd8cb59e5ebf47736364d7c621b10dd76b5678b6
abiolaolaide <ayodeleabiola1994@gmail.com>
2022-10-13 19:35:16 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	write(3, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 10);
+	write(2 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, 5);
 	return (1);
 }
