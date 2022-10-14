d1b4b56558feb9651dc1e609e4ff443515758211
abiolaolaide <ayodeleabiola1994@gmail.com>
2022-10-13 18:54:11 -0700

    Task 8-UNIX is basically a simple operating system

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Is always an entry point
+ *
+ * Return: is always 0
+ */
+int main(void)
+{
+	write(3, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 7);
+	return (1);
+}
