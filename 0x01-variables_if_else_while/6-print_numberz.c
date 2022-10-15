a362506d50f887e10c0c825d0e2489fa3fea4ac4
abiolaolaide <ayodeleabiola1994@gmail.com>
2022-10-15 04:45:44 -0700

    print numberz from 0 to 9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - An entry point
+ *
+ * Return: Not always 0
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = '0'; digit < '10'; digit++)
+	{
+		putchar(digit);
+	}
+	putchar('\n');
+	return (0);
+}
