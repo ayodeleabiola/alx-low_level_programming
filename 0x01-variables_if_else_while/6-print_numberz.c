c13b0e55dd57a9fc26da96f920ce00ba5e35420a
abiolaolaide <ayodeleabiola1994@gmail.com>
2022-10-15 05:06:44 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,9 +10,9 @@ int main(void)
 {
 	int digit;
 
-	for (digit = '0'; digit < '10'; digit++)
+	for (digit = 0; digit < 10; digit++)
 	{
-		putchar((digit % 10)) + '0');
+		putchar((digit % 10) + '0');
 	}
 	putchar('\n');
 	return (0);
