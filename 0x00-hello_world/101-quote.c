21d180b49b1cd37b8096d1bf6040ef274f5ff4ec
abiolaolaide <ayodeleabiola1994@gmail.com>
2022-10-16 02:09:13 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
- * main - Is always an entry point
+ * main - must be the first line
  *
- * Return: is 1
+ * Return: Alwways 0
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 5);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
-
