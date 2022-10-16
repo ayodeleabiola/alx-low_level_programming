02e9114429f535d98562f060d835d1eca9e46907
abiolaolaide <ayodeleabiola1994@gmail.com>
2022-10-16 03:10:11 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,10 +14,16 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if ((n % 10) > 5)
+	{
 		printf("%s %d is %d and is greater than 5\n", str, n, n % 10);
-	else if ((n % 10 == 0)
+	}
+	else if ((n % 10) == 0)
+	{
 		printf("%s %d is %d and is 0\n", str, n, n % 10);
+	}
 	else
+	{
 		printf("%s %d is %d and is less than 6 and not 0\n", str, n, n % 10);
+	}
 	return (0);
 }
