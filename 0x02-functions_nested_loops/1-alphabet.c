c204523aa96ae8ddeb178a1ce7df59857fc5b25f
abiolaolaide <ayodeleabiola1994@gmail.com>
2022-10-19 04:22:23 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Alway the starting point.
+ *
+ * Return:Always 0
+ */
+int void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
