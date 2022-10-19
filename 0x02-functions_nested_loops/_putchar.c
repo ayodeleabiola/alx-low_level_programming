060cd5cecaf499f891f4837a673155d5d02186ba
abiolaolaide <ayodeleabiola1994@gmail.com>
2022-10-19 04:07:03 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,11 @@
+include "main.h"
+/**
+ * _putchar - writes the character c to stdout
+ *  @c: The character to print
+ *
+ *  Return: is 1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
