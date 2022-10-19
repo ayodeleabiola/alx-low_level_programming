1081e816549194f594ee6bf629f37843c16374a5
abiolaolaide <ayodeleabiola1994@gmail.com>
2022-10-19 12:51:47 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,7 +11,7 @@ int print_last_digit(int num)
 	int last_digit;
 
 	last_digit = num % 10;
-	if (last < 0)
+	if (last_digit < 0)
 		last_digit = last_digit * -1;
 	_putchar(last_digit + '0');
 	return (last_digit);
