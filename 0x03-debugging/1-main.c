76503410ce4fe66f783ee44ead578fcf8bac718a
abiolaolaide <ayodeleabiola1994@gmail.com>
2022-10-20 07:23:04 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,11 +7,9 @@
 
 int main(void)
 {
-
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-
 	i = 0;
 	/**
 	 * while (i < 10)
@@ -20,6 +18,5 @@ int main(void)
 	 *}
 	 */
     	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
