55048cf36205b24978d5efc32c7aec52c7d13038
abiolaolaide <ayodeleabiola1994@gmail.com>
2022-10-20 09:33:32 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_isupper - checks if a character is upper
+ *@c: variable test
+ *Return: Always 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
