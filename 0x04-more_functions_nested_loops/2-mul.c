37257eb0226deb278d77873f35c583e8cf3f7bb2
abiolaolaide <ayodeleabiola1994@gmail.com>
2022-10-20 22:26:45 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *mul - multipky two integer
+ *@a: first int
+ *@b: second int
+ *Return: return the multiple of m and n
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
