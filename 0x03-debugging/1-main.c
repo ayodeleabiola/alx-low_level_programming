a89719ee0e5f89d16fc41958dc928a89ee1df92a
abiolaolaide <ayodeleabiola1994@gmail.com>
2022-10-22 12:26:29 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,1 +1,22 @@
+#include "main.h"
+/**
+ *main -cause an infinite loop
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	printf("infinite loop incoming :(\n");
+	/**
+	 *while(i < 10)
+	 {
+		putchar(i);
+	 }
+	*/
+	printf("infinite loopn avoided! \\o/\n");
+	return (0);
+}
 
--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -7,34 +7,6 @@
  * Return: largest number
  */
 
-int largest_number(int a, int b, int c)
-{
-	int largest;
-	
-	if (a > b)
-	{
-		if (b > c)
-		{
-			largest = a;
-		}
-		else if (a > c)
-		{
-			largest = a;
-		}
-		else
-			largest = c;
-	else
-	{
-		if (b < c)
-		{
-			largest = c;
-		}
-		else
-		{
-			largest = b;
-		}
-	}
-	return (largest);
 }
 	
  
