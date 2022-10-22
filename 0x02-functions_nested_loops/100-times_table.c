742d260863908c721e7e89b8c0f3ffd3636e6590
abiolaolaide <ayodeleabiola1994@gmail.com>
2022-10-22 10:58:08 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *print_times_table - Entry point
+ * @num: input
+ *Return: Always 0
+ */
+void print_times_table(int num)
+{
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ *main - check your code
+ *
+ * Return: 0
+ */
+int main(void)
+{
+return (0);
+}
+
