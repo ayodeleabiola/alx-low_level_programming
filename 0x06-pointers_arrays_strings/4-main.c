d8f2d5a3cdd7a9df5b386c2c66051a0461d92915
abiolaolaide <ayodeleabiola1994@gmail.com>
2022-10-26 11:44:45 -0700

    4-rev_array

--- 0x06-pointers_arrays_strings/4-main.c
+++ 0x06-pointers_arrays_strings/4-main.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdui.h>
+#include <stdio.h>
 /**
  *print_array - check code for Alx students
  *@a: an array of integers
@@ -30,6 +30,8 @@ void print_array(int *a, int n)
  */
 int main(void)
 {
+	int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
+
 	print_array(a, sizeof(a) / sizeof(int));
 	reverse_array(a, sizeof(a) / sizeof(int));
 	print_array(a, sizeof(a) / sizeof(int));
---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *reverse_array - A function that prints string in reverse order
+ *@a: input array
+ *@n: input number of elements
+ *Return: rev
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < n; i++)
+	{
+		n--;
+		j = a[i];
+		a[i] = a[n];
+		a[n] = j;
+	}
+}
