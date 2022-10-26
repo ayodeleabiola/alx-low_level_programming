0a60c4d041fc0909aba1a58e707863af734b7abd
abiolaolaide <ayodeleabiola1994@gmail.com>
2022-10-26 08:01:31 -0700

    1-strncat

--- 0x06-pointers_arrays_strings/1-main.c
+++ 0x06-pointers_arrays_strings/1-main.c
@@ -22,3 +22,4 @@ int main(void)
 	printf("%s", s2);
 	printf("%s", ptr);
 	return (0);
+}
---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *_strncpy 
+ *
+ *@dest: destination
+ *@src: source
+ *n: number
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+
