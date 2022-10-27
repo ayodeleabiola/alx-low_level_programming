f39686e6b7a7a0a96ecad3b2873470b4cab53426
abiolaolaide <ayodeleabiola1994@gmail.com>
2022-10-27 02:13:54 -0700

    100-rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,44 @@
+#include  "main.h"
+/**
+ *rot13 -  function that encode a string rot3
+ *
+ *@str: integerto be printed
+ *Return: number
+ */
+char *rot13(char *str)
+{
+int index1 = 0, index2;
+
+char alpha[52] = {
+'A', 'B', 'C', 'D', 'E', 'F',
+'G', 'H', 'I', 'J', 'K', 'L',
+'M', 'N', 'O', 'P', 'Q', 'R',
+'S', 'T', 'U', 'V', 'W', 'X',
+'Y', 'Z', 'a', 'b', 'c', 'd',
+'e', 'f', 'g', 'h', 'i', 'j',
+'k', 'l', 'm', 'n', 'o', 'p',
+'q', 'r', 's', 't', 'u', 'v',
+'w', 'x', 'y', 'z'};
+	char rot13[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+			  'T', 'U', 'V', 'W', 'X', 'Y',
+			  'Z', 'A', 'B', 'C', 'D', 'E',
+			  'F', 'G', 'H', 'I', 'J', 'K',
+			  'L', 'M', 'n', 'o', 'p', 'q',
+			  'r', 's', 't', 'u', 'v', 'w',
+			  'x', 'y', 'z', 'a', 'b', 'c',
+			  'd', 'e', 'f', 'g', 'h', 'i',
+			  'j', 'k', 'l', 'm'};
+	while (str[index1])
+	{
+		for (index2 = 0; index2 < 52; index2++)
+		{
+			if (str[index1] == alpha[index2])
+			{
+				str[index1] = rot13[index2];
+				break;
+			}
+		}
+		index1++;
+	}
+	return (str);
+}
