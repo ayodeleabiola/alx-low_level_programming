dcba02fca6156912f07a923ee5d74c5a4478e6c2
abiolaolaide <ayodeleabiola1994@gmail.com>
2022-11-08 08:44:41 -0800

    1-strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,20 +13,20 @@
  */
 char *_strdup(char *str)
 {
-	int i, end;
-	char *array;
+	char *cpy;
+	int index, len;
 
 	if (str == NULL)
 		return (NULL);
-	for (end = 0; end <= *str; end++)
-	{
-
-	}
-	end += 1;
-	array = malloc(sizeof(char) * end);
-	for (i = 0; i < end; i++)
-		array[i] = str[i];
-	if (array == NULL)
+	for (index = 0; str[index]; index++)
+		len++;
+	cpy = malloc(sizeof(char) * (len + 1));
+	if (cpy == NULL)
 		return (NULL);
-	return (array);
+	for (index = 0; str[index]; index++)
+	{
+		cpy[index] = str[index];
+	}
+	cpy[len] = '\0';
+	return (cpy);
 }
