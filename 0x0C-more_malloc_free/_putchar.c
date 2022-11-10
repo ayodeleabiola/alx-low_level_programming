9a3098976ebd24118f6d86a1f3c71e3895f8f552
abiolaolaide <ayodeleabiola1994@gmail.com>
2022-11-10 07:31:04 -0800

    0-malloc_checked

---
+++ 0x0C-more_malloc_free/_putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *_putchar - Function that print character to stdout
+ *
+ *Return: return 1 if postive, else -1 
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, -1);
+}
