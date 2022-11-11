41f105e9493ae44c420c4aa745f9a803b6c28eb3
abiolaolaide <ayodeleabiola1994@gmail.com>
2022-11-11 08:46:50 -0800

    101-mul

--- 0x0C-more_malloc_free/_putchar.c
+++ 0x0C-more_malloc_free/_putchar.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdlib.h>
+#include <unistd.h>
 /**
  *_putchar - Function that print character to stdout
  *
@@ -7,5 +7,5 @@
  */
 int _putchar(char c)
 {
-	return (write(1, &c, -1);
+	return (write(1, &c, 1));
 }
