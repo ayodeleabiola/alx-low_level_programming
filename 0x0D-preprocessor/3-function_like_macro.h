544886856f6c4990bb815aa65ba29662b65b1c96
abiolaolaide <ayodeleabiola1994@gmail.com>
2022-11-14 17:02:07 -0800

    4-sum.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,10 +1,10 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
+#ifndef FUNCTION_LIKE_MACRO
+#define FUNCTION_LIKE_MACRO
 /**
  *File: 3-function_like_macro.h
  *Auth: Ayodele Abiola
  *Desc: Header file that defines a function-like macro ABS(x)
  *	that computes the absolute value of a number x.
  */
-#define ABS(x) ((x) < 0 ? (-x) : (x))
+#define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
 #endif
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -6,5 +6,7 @@
  *Desc: Header file that defines a function-like macro SUM(x, y)
  *	that computes the sum of the numbers x and y.
  */
+
 #define SUM(x, y) ((x) + (y))
+
 #endif
