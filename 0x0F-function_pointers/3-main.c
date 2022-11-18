e780107a7eb173ecab55d24cb4d7501838d98157
abiolaolaide <ayodeleabiola1994@gmail.com>
2022-11-18 11:23:39 -0800

    3-main, 3-op_functions, 3-get_op_func, 3-calc

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,32 +8,29 @@
  *
  *Return: Always 0.
  */
-int main(int __attribute__((__unused__)) argc, char *argv)
+int main(int argc, char *argv[])
 {
-	int one, two, ans;
-	int (*res)(int, int);
-	char *get_op;
+	int a, b;
+	int (*operation)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	one = atoi(argv[1]);
-	two = atoi(argv[3]);
-	get_op = argv[2];
-	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
+	if (argv[2][1])
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((*get_op == '/' || *get_op == '%') && (*argv[3] == '0'))
+	operation = get_op_func(argv[2]);
+	if (operation == NULL)
 	{
 		printf("Error\n");
-		exit(100);
+		exit(99);
 	}
-	res = get_op_func(get_op);
-	ans = res(one, two);
-	printf("%d\n", ans);
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	printf("%d\n", operation(a, b));
 	return (0);
 }
