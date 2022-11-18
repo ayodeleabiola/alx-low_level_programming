329278ae7c1919a07ec71f11f8aa8765e83e0a6f
abiolaolaide <ayodeleabiola1994@gmail.com>
2022-11-18 09:09:01 -0800

    0-sum_them_all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,16 +1,16 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 /**
- *sum_them_all - Returns the sum of all its paramters.
- *@n: The number of paramters passed to the function.
- *@...: A variable number of paramters to calculate the sum of.
+ * sum_them_all - Returns the sum of all its paramters.
+ * @n: The number of paramters passed to the function.
+ * @...: A variable number of paramters to calculate the sum of.
  *
- *Return: If n == 0 - 0.
- *	Otherwise - the sum of all parameters.
+ * Return: If n == 0 - 0.
+ *         Otherwise - the sum of all parameters.
  */
-
 int sum_them_all(const unsigned int n, ...)
 {
+
 	va_list nums;
 	unsigned int index, sum = 0;
 
@@ -19,5 +19,4 @@ int sum_them_all(const unsigned int n, ...)
 		sum += va_arg(nums, int);
 	va_end(nums);
 	return (sum);
-
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,26 +1,37 @@
 #ifndef VARIADIC_FUNCTIONS_H
+
 #define VARIADIC_FUNCTIONS_H
-/**
- *Auth: Ayodele Abiola
- *Describe: An header file containing prototype of 
- *function for the header file use in the 0x010_variadic_function directory
+
+
+
+/*
+
+ * File: variadic_functions.h
+
+ * Auth: Johnpromise Ononuju
+
+ * Desc: Header file containing prototypes for all functions
+
+ *       used in the 0x0F-variadic_functions directory.
+
  */
- #include <stdarg.h>
+
+
+
+#include <stdarg.h>
 /**
-i *struct printer: A new struct defining a printer
- *@symbol: A symbol representing a data type.
- *@print: A function pointer to a function that prints
- *	 a data type corresponding to symbol.
+ * struct printer - A new struct type defining a printer.
+ * @symbol: A symbol representing a data type.
+ * @print: A function pointer to a function that prints
+ *         a data type corresponding to symbol.
  */
 typedef struct printer
 {
 	char *symbol;
 	void (*print)(va_list arg);
-}print_t;
-int _putchar(char c);
+} printer_t;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
 #endif
