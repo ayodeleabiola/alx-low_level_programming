62fe61ae412a500ead1d9b7fa43423eb6afbb511
abiolaolaide <ayodeleabiola1994@gmail.com>
2022-11-18 08:57:33 -0800

    0-sum_them_all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,6 +8,7 @@
  *Return: If n == 0 - 0.
  *	Otherwise - the sum of all parameters.
  */
+
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list nums;
@@ -18,4 +19,5 @@ int sum_them_all(const unsigned int n, ...)
 		sum += va_arg(nums, int);
 	va_end(nums);
 	return (sum);
+
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -16,8 +16,8 @@ typedef struct printer
 {
 	char *symbol;
 	void (*print)(va_list arg);
-}print_t
-
+}print_t;
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
