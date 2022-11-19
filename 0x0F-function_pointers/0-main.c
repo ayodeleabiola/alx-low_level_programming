afe5b2bb8037b3bd1150b8559876d94af6dad2d3
abiolaolaide <ayodeleabiola1994@gmail.com>
2022-11-18 17:53:30 -0800

    0-main.c

--- 0x0F-function_pointers/0-main.c
+++ 0x0F-function_pointers/0-main.c
@@ -9,12 +9,10 @@
 void print_name_as_is(char *name)
 {
     printf("Hello, my name is %s\n", name);
+
 }
-
 /**
-
  * print_name_uppercase - print a name in uppercase
-
  * @name: name of the person
  *
  * Return: Nothing.
@@ -22,6 +20,7 @@ void print_name_as_is(char *name)
 void print_name_uppercase(char *name)
 {
     unsigned int i;
+
     printf("Hello, my uppercase name is ");
     i = 0;
     while (name[i])
